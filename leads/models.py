1952e2aae78a23fef4ed16b59ddb7817c804b26b
Peagah <peagahvieira2003@gmail.com>
2023-07-16 02:45:54 -0300

    feat: Lead model create

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    age = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
