ff6ea76fb58f4d4eb517b65f01fe5fe816e65790
Peagah <peagahvieira2003@gmail.com>
2023-07-17 14:30:02 -0300

    feat: Leads - HTTP methods documentation

--- leads/views.py
+++ leads/views.py
@@ -7,3 +7,33 @@ class LeadAPIViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+            Return a list of all Leads from database.
+        """
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """
+            Create a Lead on database.
+        """
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+            Return a Lead from database.
+        """
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+            Applies partial update to a Lead.
+        """
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+            Delete a Lead from database.
+        """
+        return super().destroy(request, *args, **kwargs)
