e4ca8d4d46feb5b6140f11e1f46fcd8a5ebc92be
Peagah <peagahvieira2003@gmail.com>
2023-07-17 14:24:10 -0300

    feat: Agents - HTTP methods documentation

--- agents/views.py
+++ agents/views.py
@@ -7,3 +7,33 @@ class AgentAPIViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
     queryset = Agent.objects.all()
     serializer_class = AgentSerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+            Return a list of all Agents from database.
+        """
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """
+            Create a Agent on database.
+        """
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+            Return a Agent from database.
+        """
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+            Applies partial update to a Agent.
+        """
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+            Delete a Agent from database.
+        """
+        return super().destroy(request, *args, **kwargs)
