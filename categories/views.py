f26a84aed7009e43b36163510c45ae127ccc4656
Peagah <peagahvieira2003@gmail.com>
2023-07-17 13:25:42 -0300

    feat: Categories - HTTP methods documentation

--- categories/views.py
+++ categories/views.py
@@ -7,3 +7,33 @@ class CategoryAPIViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def list(self, request, *args, **kwargs):
+        """
+            Return a list of all Categories from database.
+        """
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """
+            Create a Category on database.
+        """
+        return super().create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+            Return a Category from database.
+        """
+        return super().retrieve(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+            Applies partial update to a Category.
+        """
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+            Delete a Category from database.
+        """
+        return super().destroy(request, *args, **kwargs)
